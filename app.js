8a98fb06c0072f2a80896a39526b3299b63733ef
Lusine <lus21484@gmail.com>
2018-01-15 16:49:16 +0400

    404 added

--- app.js
+++ app.js
@@ -83,6 +83,23 @@ app.get('/api/users', (req, res) => {
     res.json(users);
 });
 
+//404 Page
+app.use((req, res, next) => {
+    res.status(404);
+
+    res.format({
+        html: function () {
+            res.render('404', { url: req.url })
+        },
+        json: function () {
+            res.json({ error: 'Not found' })
+        },
+        default: function () {
+            res.type('txt').send('Not found')
+        }
+    })
+});
+
 app.listen(3000, () => {
     console.log('server started')
 });
